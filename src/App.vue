d346975c03851b343c4b12cdae9929dabf78317d
Chinatsu <0726dogdog@gmail.com>
2026-03-20 15:48:45 +0900

    投稿機能を実装

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <template>
   <div>
     <h1>まごレター</h1>
+
+    <!-- 入力 -->
+    <input v-model="message" placeholder="メッセージを書いてね" />
+    <button @click="addMessage">投稿</button>
+
+    <!-- 一覧表示 -->
+    <ul>
+      <li v-for="(msg, index) in messages" :key="index">
+        {{ msg }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const message = ref("");
+const messages = ref([]);
+
+const addMessage = () => {
+  if (message.value !== "") {
+    messages.value.push(message.value);
+    message.value = "";
+  }
+};
+</script>
+
